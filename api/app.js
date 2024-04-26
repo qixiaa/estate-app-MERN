46f4d2f0ada2f896ad1ae9dc88a91c50684ee919
qixiaa <aqixiaa@gmail.com>
2024-04-26 15:04:11 +0800

    update user ui/routes/controller

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Server is running.");
---
+++ api/routes/user.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controller/user.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.get("/:id", verifyToken, getUser);
+
+router.put("/:id", verifyToken, updateUser);
+
+router.delete("/:id", verifyToken, deleteUser);
+
+export default router;
